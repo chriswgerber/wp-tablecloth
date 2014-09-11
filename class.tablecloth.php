55ddb195e7a73459db508bd27ab5253d08068027
Christopher Gerber <chriswgerber@gmail.com>
2014-09-11 01:24:58 -0500

    Added a readme.

--- class.tablecloth.php
+++ class.tablecloth.php
@@ -3,7 +3,7 @@
  * Plugin Name: TableCloth
  * Plugin URI: http://wwww.chriswgerber.com/tablecloth-plugin/
  * Description: A plugin for adding table styling using tablecloth.js, created by Brian Sewell
- * Version: 0.0.1
+ * Version: 0.0.1-alpha
  * Author: Christopher Gerber
  * Author URI: http://www.chriswgerber.com/
  * License: GPL2
---
+++ readme.md
@@ -0,0 +1,14 @@
+TableCloth - WordPress
+==========================================
+
+A plugin for WordPress that utilizes Brian Sewell's Tablecloth.js.
+
+## [Tablecloth.js][tablecloth] ##
+
+If you have any questions, feel free to contact me.
+
+## Version ##
+
+\# 0.0.1-alpha
+
+[tablecloth]: https://github.com/bwsewell/tablecloth
