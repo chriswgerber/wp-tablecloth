e2d9c98543fd7399d0e17b997f279edff23df9d0
Christopher Gerber <chriswgerber@gmail.com>
2014-09-11 00:24:23 -0500

    Initial Commit. Added Tablecloth.js as submodule.

---
+++ class.tablecloth.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: TableCloth
+ * Plugin URI: http://wwww.chriswgerber.com/tablecloth-plugin/
+ * Description: A plugin for adding table styling using tablecloth.js, created by Brian Sewell
+ * Version: 0.0.1
+ * Author: Christopher Gerber
+ * Author URI: http://www.chriswgerber.com/
+ * License: GPL2
+ */
